50b959ab616df5cc8f54c2f07c3e32f1094efbcf
Austin Craft <austincrft@gmail.com>
2015-08-29 15:35:11 -0500

    various template changes

--- app/views.py
+++ app/views.py
@@ -3,6 +3,20 @@ from flask import render_template
 
 
 @app.route('/')
-@app.route('/home')
 def home():
     return render_template('home.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
