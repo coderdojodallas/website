0268ff938a2d22344bf232c8f88c33c91a70e81c
Craft, Austin <austin.craft@tylertech.com>
2015-10-27 18:19:02 -0500

    added messages.py, closes #10

---
+++ app/messages.py
@@ -0,0 +1,43 @@
+AGE_GROUP_VALIDATION = (
+    "You must select at least one age group."
+)
+
+EMAIL_ADDRESS_SUBMITTED_AND_CONFIRMED = (
+    "The email address '{0}' has already been submitted and confirmed. You will receive future CoderDojo Dallas emails."
+)
+
+EMAIL_ADDRESS_SUBMITTED_NOT_CONFIRMED = (
+    "The email address '{0}' has already been submitted, but not confirmed. Check your inbox for an email with confirmation steps."
+)
+
+CONFIRMATION_EMAIL_SENT = (
+    "Confirmation email sent to '{0}'."
+)
+
+CONFIRMATION_LINK_INVALID = (
+    "The confirmation link is invalid or has expired. Please fill out the Mailing List form again."
+)
+
+CONFIRMATION_LINK_ALREADY_CONFIRMED = (
+    "The email address '{0}' has already been confirmed"
+)
+
+CONFIRMATION_LINK_CONFIRMED = (
+    "The email address '{0}' has successfully been added to our mailing list. Thank you for your interest in CoderDojo Dallas!"
+)
+
+MAILING_LIST_PREFERENCES_ERROR = (
+    "The mailing list preferences for your email could not be loaded. Please contact help@coderdojodallas.com so we can assist in updating your mailing list preferences."
+)
+
+MAILING_LIST_PREFERENCES_SUCCESS = (
+    "Your preferences have been successfully updated."
+)
+
+MAILING_LIST_UNSUBSCRIBE_ERROR = (
+    'There was an error unsubscribing your email address. Please contact help@coderdojodallas.com so we can assist you in unsubscribing.'
+)
+
+MAILING_LIST_UNSUBSCRIBE_SUCCESS = (
+    "You have been successfully unsubscribed from the mailing list."
+)
